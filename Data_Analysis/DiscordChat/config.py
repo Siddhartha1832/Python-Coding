8566f48e1e338dd838954bc0da76c925e6ea1cac
Akash Jeez <akashit63@gmail.com>
2019-03-30 09:39:58 +0530

    Update config.py

--- Data_Analysis/DiscordChat/config.py
+++ Data_Analysis/DiscordChat/config.py
@@ -1,9 +1,8 @@
 ## Discord Developer Portal — https://discordapp.com/developers
 ## Discord Login - https://discordapp.com/
-## My Discord Credentials (Username - akashjeez@icloud.com / Password - akash@hts)
 
 # I created discored_sever as akjzserver and created application valled testsbot
 
-discord_token = 'NTU3NTU4ODczNjc0ODc0ODgx.D3KDTQ.AjY7Ok8c9QQDpnVvcdwS-NPCMQg' # test_bot
-discord_client_id = '557558873674874881'
-discord_client_secret = 'VBMqWO2S7vrEdDsb-YOMBS_nwweBJzCc'
+discord_token = ''
+discord_client_id = ''
+discord_client_secret = ''
